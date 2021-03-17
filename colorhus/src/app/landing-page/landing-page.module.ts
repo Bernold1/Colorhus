609bdf050a2d70346b9dec70543ac2c9c9ec6c6a
Bernold <christopherbernold@icloud.com>
2021-03-17 16:52:27 +0100

    Landing page finished.

--- colorhus/src/app/landing-page/landing-page.module.ts
+++ colorhus/src/app/landing-page/landing-page.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingPageRoutingModule, MatIconModule],
+  imports: [
+    CommonModule,
+    LandingPageRoutingModule,
+    MatIconModule,
+    MatCardModule,
+  ],
 })
 export class LandingPageModule {}
